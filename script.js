d9d4064c6de868f1982b03aab803edd1398ff3ef
ShaunMpfuni <shaun.mpfuni@gmail.com>
2023-11-02 16:36:32 +0200

    modified homepage and changed search button

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
  
+        // function to change color of navigation
+
+         
         // Get all the navigation tabs
         const navTabs = document.querySelectorAll('.nav-item');
 
@@ -18,6 +21,10 @@
         navTabs.forEach(tab => {
           tab.addEventListener('click', handleTabClick);
         });
+
+
+
+
 
         //function to call frames to the main body
          document.addEventListener('DOMContentLoaded', function () {
@@ -40,3 +47,46 @@
                 });
         });
 
+
+
+        // search function
+
+        document.addEventListener("DOMContentLoaded", function() {
+            const menuItems = document.getElementById("menuItems");
+            const searchInput = document.getElementById("searchInput");
+            const noMatchMessage = document.getElementById("noMatchMessage");
+
+            document.getElementById("searchForm").addEventListener("submit", function(event) {
+                event.preventDefault();
+                const searchTerm = searchInput.value.toLowerCase();
+                let matchFound = false;
+
+                // Iterate through both top-level and dropdown menu items
+                const allMenuItems = Array.from(menuItems.querySelectorAll("li"));
+                for (const menuItem of allMenuItems) {
+                    const menuText = menuItem.textContent.toLowerCase();
+
+                    if (menuText.includes(searchTerm)) {
+                        // If the menu item is in the dropdown, open the dropdown first
+                        if (menuItem.classList.contains("dropdown-item")) {
+                            const parentDropdown = menuItem.closest(".dropdown");
+                            if (parentDropdown) {
+                                parentDropdown.querySelector(".dropdown-toggle").click();
+                            }
+                        }
+
+                        // Scroll to the found menu item
+                        menuItem.querySelector("a").click();
+                        matchFound = true;
+                        break; // Stop searching after the first match
+                    }
+                }
+
+                // Display no match message if no matching menu item is found
+                if (!matchFound) {
+                    noMatchMessage.style.display = "block";
+                } else {
+                    noMatchMessage.style.display = "none";
+                }
+            });
+        });
